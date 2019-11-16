c4fd25e7800a65bbb70dee252e340cc3d669ec32
Shivang Bokolia <sb8392@g.rit.edu>
2019-11-16 15:11:35 -0500

    Update JottGrammar.java
    JottGrammar added.

--- src/parsing/JottGrammar.java
+++ src/parsing/JottGrammar.java
@@ -21,19 +21,28 @@ public class JottGrammar {
         grammar.put("digit", new String[][]{{"0"}, {"1"}, {"2"}, {"3"}, {"4"}, {"5"}, {"6"}, {"7"}, {"8"}, {"9"}});
         grammar.put("sign", new String[][]{{"-"}, {"+"}, {""}});
         grammar.put("id", new String[][]{{"l_char", "char*"}});
-        grammar.put("stmt", new String[][]{{"print_stmt"}, {"asmt"}, {"expr", "end_statement"}});
+        grammar.put("stmt", new String[][]{{"print_stmt"}, {"asmt"},{"if", "start_paren", "expr", "end_paren", "{", "b_stmt_list", "}", "else", "{", "b_stmt_list", "}"},
+                {"if", "(", "expr", ")", "{", "b_stmt_list", "}"},
+                {"while", "start_paren", "i_expr", "end_paren", "{", "b_stmt_list", "}"},
+                {"for", "start_paren", "asmt", "i_expr", ";", "r_asmt", "end_paren", "{", "b_stmt_list", "}"}, {"r_asmt", "end_statement"}, {"expr", "end_statement"}});
         grammar.put("expr", new String[][]{{"i_expr"}, {"d_expr"}, {"s_expr"}, {"id"}});
         grammar.put("print_stmt", new String[][]{{"print", "start_paren", "expr", "end_paren", "end_statement"}});
         grammar.put("asmt", new String[][]{{"Double", "id", "=", "d_expr", "end_statement"}, {"Integer", "id", "=", "i_expr", "end_statement"}, {"String", "id", "=", "s_expr", "end_statement"}});
-        grammar.put("op", new String[][]{{"+"}, {"*"}, {"/"}, {"-"}, {"^"}});
+        grammar.put("op", new String[][]{{"+"}, {"*"}, {"/"}, {"-"}, {"^"}, {"rel_op"}});
         grammar.put("dbl", new String[][]{{"sign", "digit*", ".", "digit", "digit*"}});
         grammar.put("d_expr", new String[][]{{"id"}, {"dbl"}, {"dbl", "op", "dbl"}, {"dbl", "op", "d_expr"}, {"d_expr", "op", "dbl"}, {"d_expr", "op", "d_expr"}});
         grammar.put("int", new String[][]{{"sign", "digit", "digit*"}});
         grammar.put("i_expr", new String[][]{{"id"}, {"int"}, {"int", "op", "int"}, {"int", "op", "i_expr"}, {"i_expr", "op", "int"}, {"i_expr", "op", "i_expr"}});
+        grammar.put("i_exp", new String[][]{{"i_expr", "rel_op", "i_expr"}, {"s_expr", "rel_op", "s_expr"}, {"d_expr", "rel_op", "d_expr"}});
         grammar.put("str_literal", new String[][]{{"\"str\""}});
         grammar.put("str", new String[][]{{"char", "str"}, {"space", "str"}, {""}});
-        grammar.put("s_expr", new String[][]{{"str_literal"}, {"id"}, {"concat", "start_paren", "s_expr", ",", "s_expr", "end_paren"}, {"charAt", "start_paren", "s_expr", ",", "i_expr", "end_paren"}});
-        grammar.put("char*", new String[][]{{"char", "char*"}, {""}});
-        grammar.put("digit*", new String[][]{{"digit", "digit*"}, {""}});
+        grammar.put("s_expr", new String[][]{{"str_literal"}, {"id"}, {"concat", "start_paren", "s_expr", ",", "s_expr", "end_paren"}, {"charAt", "start_paren", "s_expr", ",", "i_expr", "end_paren"}, {"s_expr", "op", "s_expr"}});
+        grammar.put("rel_op", new String[][]{{">"}, {"<"}, {">="}, {"<="}, {"=="}, {"!="}});
+        grammar.put("b_stmt_list", new String[][]{{"b_stmt", "b_stmt_list"}, {""}});
+        grammar.put("b_stmt", new String[][]{{"r_asmt", "end_statement"}, {"print_stmt"}, {"if", "start_paren", "expr", "end_paren", "{", "b_stmt_list", "}", "else", "{", "b_stmt_list", "}"},
+                {"if", "(", "expr", ")", "{", "b_stmt_list", "}"},
+                {"while", "start_paren", "i_expr", "end_paren", "{", "b_stmt_list", "}"},
+                {"for", "start_paren", "asmt", "i_expr", ";", "r_asmt", "end_paren", "{", "b_stmt_list", "}"}, {"expr", "end_statement"}});
+        grammar.put("r_asmt", new String[][]{{"id", "=", "expr"}});
     }
 }
